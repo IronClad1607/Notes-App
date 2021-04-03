31eb2aba7a4ac01f4cdf3b442e9bd3cb0446ead2
Ishaan Jain <ishaan160799@gmail.com>
2021-04-03 20:06:58 +0530

    basic architecture ready

--- app/src/main/java/com/ironclad/notesapp/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/ironclad/notesapp/view/fragments/HomeFragment.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ironclad.notesapp.R
 import com.ironclad.notesapp.databinding.FragmentHomeBinding
 
@@ -27,8 +27,7 @@ class HomeFragment : Fragment() {
         handleMenu()
 
         binding?.buttonAdd?.setOnClickListener {
-            //todo nav to add fragment
-            Toast.makeText(requireContext(), "Add Click", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(HomeFragmentDirections.goToAddANote())
         }
     }
 
@@ -36,8 +35,7 @@ class HomeFragment : Fragment() {
         binding?.toolbarHome?.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.item_profile -> {
-                    //todo nav to profile fragment
-                    Toast.makeText(requireContext(), "Profile Click", Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(HomeFragmentDirections.goToProfile())
                     true
                 }
                 else -> {
---
+++ app/src/main/java/com/ironclad/notesapp/view/fragments/ProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.ironclad.notesapp.view.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ironclad.notesapp.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private var binding: FragmentProfileBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
+}
