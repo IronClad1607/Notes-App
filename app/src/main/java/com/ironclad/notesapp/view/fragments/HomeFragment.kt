7c65e0a22ca5842d23051466b53ff9bbfc5dbf67
Ishaan Jain <ishaan160799@gmail.com>
2021-04-03 19:59:20 +0530

    home layout

--- app/src/main/java/com/ironclad/notesapp/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/ironclad/notesapp/view/fragments/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.ironclad.notesapp.R
 import com.ironclad.notesapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,10 +19,33 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-
         return binding?.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleMenu()
+
+        binding?.buttonAdd?.setOnClickListener {
+            //todo nav to add fragment
+            Toast.makeText(requireContext(), "Add Click", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun handleMenu() {
+        binding?.toolbarHome?.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.item_profile -> {
+                    //todo nav to profile fragment
+                    Toast.makeText(requireContext(), "Profile Click", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
 
     override fun onDestroy() {
         binding = null
