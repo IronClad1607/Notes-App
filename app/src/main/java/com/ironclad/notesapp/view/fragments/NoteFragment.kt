e8d6106dac5bbf87f6d6073be4b27e341b563d83
Ishaan Jain <ishaan160799@gmail.com>
2021-05-05 17:25:57 +0530

    note fragment complete

--- app/src/main/java/com/ironclad/notesapp/view/fragments/NoteFragment.kt
+++ app/src/main/java/com/ironclad/notesapp/view/fragments/NoteFragment.kt
@@ -1,11 +1,14 @@
 package com.ironclad.notesapp.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.ironclad.notesapp.R
 import com.ironclad.notesapp.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
@@ -27,6 +30,23 @@ class NoteFragment : Fragment() {
         val note = args.note
 
         binding?.toolbarNote?.title = note.title
+        binding?.textViewContent?.text = note.message
+        binding?.toolbarNote?.background = when (note.label) {
+            "Passwords" -> setColor(R.color.violet)
+            "Shopping List" -> setColor(R.color.blue)
+            "Task" -> setColor(R.color.red)
+            "Messages" -> setColor(R.color.yellow)
+            "List" -> setColor(R.color.indigo)
+            "Ideas" -> setColor(R.color.green)
+            "Other" -> setColor(R.color.orange)
+            else -> setColor(R.color.notes_green)
+        }
+
+        binding?.textViewUpdated?.text = note.updateAt
+    }
+
+    private fun setColor(color: Int): Drawable? {
+        return ContextCompat.getDrawable(requireContext(), color)
     }
 
     override fun onDestroy() {
