d6b8949183e53bc45e37058412de9bc0c403e45b
Ishaan Jain <ishaan160799@gmail.com>
2021-05-10 19:28:02 +0530

    shift to firebase

--- app/src/main/java/com/ironclad/notesapp/view/fragments/LoginFragment.kt
+++ app/src/main/java/com/ironclad/notesapp/view/fragments/LoginFragment.kt
@@ -16,6 +16,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.ironclad.notesapp.R
 import com.ironclad.notesapp.databinding.FragmentLoginBinding
 import com.ironclad.notesapp.utils.Constants.Companion.ERROR_TAG
 import com.ironclad.notesapp.utils.SharedPreferenceHelper
@@ -23,6 +28,7 @@ import com.ironclad.notesapp.utils.SharedPreferenceHelper
 class LoginFragment : Fragment() {
     private var binding: FragmentLoginBinding? = null
     private var mGoogleSignInClient: GoogleSignInClient? = null
+    private lateinit var auth: FirebaseAuth
     private lateinit var preferenceManager: SharedPreferenceHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,11 +36,14 @@ class LoginFragment : Fragment() {
         preferenceManager = SharedPreferenceHelper.getInstance(requireContext())
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .requestProfile()
             .build()
 
         mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
+        auth = Firebase.auth
     }
 
     override fun onCreateView(
@@ -90,13 +99,23 @@ class LoginFragment : Fragment() {
                 userLoggedIn = true
                 loginSkipped = false
             }
-            Toast.makeText(requireContext(), "User Logged In Successfully", Toast.LENGTH_SHORT)
-                .show()
-            findNavController().navigate(LoginFragmentDirections.goToHomeFromLogin())
+            firebaseAuthWithGoogle(account?.idToken)
         } catch (e: ApiException) {
-            Toast.makeText(requireContext(), "Log In failed.", Toast.LENGTH_SHORT).show()
             Log.e(ERROR_TAG, e.localizedMessage ?: "Failed")
         }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credentials =GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credentials)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "User Logged In Successfully", Toast.LENGTH_SHORT)
+                    .show()
+                findNavController().navigate(LoginFragmentDirections.goToHomeFromLogin())
+            }.addOnFailureListener {
+                Toast.makeText(requireContext(), "Log In failed.", Toast.LENGTH_SHORT).show()
+                Log.e(ERROR_TAG, it.localizedMessage ?: "Failed")
+            }
     }
 
     companion object {
