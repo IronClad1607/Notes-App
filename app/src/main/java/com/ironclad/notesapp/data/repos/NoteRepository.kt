6d5c876f861eea49d22b9be3cf0b98052a6aa60e
Ishaan Jain <ishaan160799@gmail.com>
2021-05-11 18:43:36 +0530

    logout complete

--- app/src/main/java/com/ironclad/notesapp/data/repos/NoteRepository.kt
+++ app/src/main/java/com/ironclad/notesapp/data/repos/NoteRepository.kt
@@ -9,4 +9,5 @@ interface NoteRepository {
     suspend fun addNote(note: Note): Long
     suspend fun updateNote(note: Note)
     suspend fun deleteNote(note: Note)
+    suspend fun clearDb()
 }
--- app/src/main/java/com/ironclad/notesapp/view/fragments/ProfileFragment.kt
+++ app/src/main/java/com/ironclad/notesapp/view/fragments/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import coil.load
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -14,18 +15,27 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ironclad.notesapp.databinding.FragmentProfileBinding
+import com.ironclad.notesapp.utils.SharedPreferenceHelper
 import com.ironclad.notesapp.utils.extensions.setUpFullHeight
+import com.ironclad.notesapp.view.viewmodels.ProfileViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ProfileFragment : BottomSheetDialogFragment() {
 
     private var binding: FragmentProfileBinding? = null
     private lateinit var auth: FirebaseAuth
+    private lateinit var preferenceManager: SharedPreferenceHelper
+
+    private val viewModel by viewModels<ProfileViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
+        preferenceManager = SharedPreferenceHelper.getInstance(requireContext())
     }
 
     override fun onCreateView(
@@ -53,7 +63,21 @@ class ProfileFragment : BottomSheetDialogFragment() {
             close.setOnClickListener {
                 findNavController().navigateUp()
             }
+
+            btnLogOut.setOnClickListener {
+                CoroutineScope(Dispatchers.Main).launch {
+                    viewModel.clearDb().also {
+                        signOut()
+                    }
+                }
+            }
         }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        preferenceManager.userLoggedIn = false
+        findNavController().navigate(ProfileFragmentDirections.goToLoginFromProfile())
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
